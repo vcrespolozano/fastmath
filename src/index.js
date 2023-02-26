8571f3302414cd1790d5a384ff6b1c28178614a9
vcrespolozano <vcrespolozano@gmail.com>
2023-02-26 09:45:17 +0100

    Publishing build.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Fastmath from './Fasthmath';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Fastmath />
-);
